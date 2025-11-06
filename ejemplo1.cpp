808862e3d4b854e668fb50c61b2fe9d9c678717e
Abel Villaseca <abelvillasecac@gmail.com>
2025-11-05 21:56:48 -0500

    first commit

---
+++ ejemplo1.cpp
@@ -0,0 +1,21 @@
+#include <iostream> // Incluir bibliotecas
+using namespace std; // Espacio de nombres
+
+int main() { // Función principal
+
+    int numero = 10;
+    float fraccion = 0.5;
+    double decimal = 0.590839;
+    char letra = 'A';
+    bool verdadero = false;// 1 = true y 0 = false
+    string texto = "Hola";
+
+    cout << "Numero: " << numero << endl;
+    cout << "Fraccion: " << fraccion << endl;
+    cout << "Decimal: " << decimal << endl;
+    cout << "Letra: " << letra << endl;
+    cout << "Verdadero: " << verdadero << endl;
+    cout << "Texto: " << texto << endl;
+
+    return 0;
+}
